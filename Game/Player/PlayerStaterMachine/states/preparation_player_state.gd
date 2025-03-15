cbee3e0d17e6dcb9e20eb838543f71d8704410ce
p-dobb <fackn1234@gmail.com>
2025-03-15 13:21:38 -0700

    Prep phase wip

--- Game/Player/PlayerStaterMachine/states/preparation_player_state.gd
+++ Game/Player/PlayerStaterMachine/states/preparation_player_state.gd
@@ -3,9 +3,18 @@ class_name PreparationPlayerState extends PlayerState
 ## set position (done via game manager possibly), and display cards
 ## NOTE: Will probably be nice to display some transition animation or whatever here so its not so abrupt
 
+## NOTE: Since we are taking 3 steps maybe each step can server a purpose:
+## First step play defensive cards
+## Second step play movement card
+## Third for offensive card
+## Not sure if this viable but just a thought
+
+
 func handle_input(event: InputEvent):
 	if event.is_action_pressed(&"prep"):
 		transition.emit(&"GroundedPlayerState")
+	elif event.is_action_pressed(&"proceed"):
+		pass
 
 func enter(msg: Dictionary = {}) -> void:
 	PLAYER.rotation = Vector3.ZERO
