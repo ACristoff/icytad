5198b53d100ff081637abd617a97bea2337a037d
p-dobb <fackn1234@gmail.com>
2025-03-22 02:40:07 -0700

    Used the right node

--- Game/UI/Scenes/PrepUI/scripts/card_ui.gd
+++ Game/UI/Scenes/PrepUI/scripts/card_ui.gd
@@ -2,13 +2,16 @@ extends CanvasLayer
 
 func _ready() -> void:
 	var tween : Tween = create_tween()
+	# Every tween after this will run in parallel
 	tween.set_parallel()
+	# Connecting tween finish signal to function
 	tween.finished.connect(_on_tween_finished)
-	tween.tween_property($Control/TextureRect, "position", $Control/Marker2D.global_position, 1).set_trans(Tween.TRANS_QUINT)
-	tween.tween_property($Control/TextureRect2, "position", $Control/Marker2D2.global_position, 1).set_trans(Tween.TRANS_QUINT)
+	
+	tween.tween_property($Control/TextureRect, "global_position", $Control/Marker2D.global_position, 1.0).set_trans(Tween.TRANS_QUINT)
+	tween.tween_property($Control/TextureRect2, "global_position", $Control/Marker2D2.global_position, 1.0).set_trans(Tween.TRANS_QUINT)
 
 
-func handle_input(event: InputEvent):
+func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"proceed"):
 		var tween : Tween = create_tween()
 		tween.set_parallel()
